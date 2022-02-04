c21be6b76fa7ab5c3fcc896291a17eec6ebea6a6
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-04 13:26:43 -0600

    feat(auth): Add User model

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    FEMALE = 'F'
+    MALE = 'M'
+    NON_BINARY = 'NB'
+
+    GENDER_CHOICES = [
+        (FEMALE, 'Female'),
+        (MALE, 'Male'),
+        (NON_BINARY, 'NON_BINARY')
+    ]
+
+    birthdate = models.DateField()
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    image_url = models.URLField(max_length=200)
