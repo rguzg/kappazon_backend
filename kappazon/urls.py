6189b716735e931d83fc4ff9b5dfef51de03a1fb
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-05 00:44:33 -0600

    feat(auth): Add API router
    - Add DefaultRouter
    - Add users route
    * Add 'api/' URL pattern

--- kappazon/urls.py
+++ kappazon/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import routers
+from apps.users.viewsets import UsersViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('users', UsersViewSet, 'users')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/', include(router.urls)),
 ]
