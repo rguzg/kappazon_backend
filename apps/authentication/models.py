ab48ee4697f52937e94e8b1c434b3e6d662cb7f6
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-04 19:09:05 -0600

    feat(auth): Create profile model
    This model stores non-auth related information about the user

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
-# Create your models here.
 class User(AbstractUser):
+    pass
+
+class Profile(models.Model):
     FEMALE = 'F'
     MALE = 'M'
     NON_BINARY = 'NB'
@@ -14,6 +15,7 @@ class User(AbstractUser):
         (NON_BINARY, 'NON_BINARY')
     ]
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthdate = models.DateField()
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
     image_url = models.URLField(max_length=200)
