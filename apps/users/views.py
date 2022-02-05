65c7dce2c6544b400f9877103aefbadeec68b77a
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-05 12:44:23 -0600

    feat(auth): Add custom TokenObtainPairView
    This custom view customizes the claims returned by the access and refresh tokens

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+
+class TokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['image_url'] = user.image_url
+        token['user_type'] = 'admin' if user.is_staff == 1 else 'customer'
+
+        return token
+
+
+class TokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
--- kappazon/urls.py
+++ kappazon/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.users.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework import routers
 from apps.users.viewsets import UsersViewSet
 
