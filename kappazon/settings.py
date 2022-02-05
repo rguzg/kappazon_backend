b2e3e4ad946a52499f998f535d33147c4514fd85
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-04 18:09:17 -0600

    feat(auth): Add Simple JWT to Rest Framework Auth Classes

--- kappazon/settings.py
+++ kappazon/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework Settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
