df01270381fa3589af406bac73c8edfa16028f7e
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-06 00:21:58 -0600

    feat(products): Add products model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    description = models.TextField(blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
+    inventory = models.PositiveIntegerField(default=0)
+    image_url = models.URLField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
