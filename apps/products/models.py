b2901d18867ca9c3689748141a1542c62c1ee43f
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-07 00:42:21 -0600

    feat(products): Add UUID primary key field to Product model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
