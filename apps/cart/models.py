1d1f505cbbb9ff23a072e13fb96acdf0305ee2af
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-07 19:24:40 -0600

    feat(cart): Add Cart and CartItem models

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from ..users.models import User
+from ..products.models import Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_price = models.PositiveIntegerField(default=0)
+    total_items = models.PositiveIntegerField(default=0)
+    products = models.ManyToManyField(Product, through='CartItem')
+
+
+class CartItem(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['cart', 'product'], name='unique_cart_item')
+        ]
+
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
