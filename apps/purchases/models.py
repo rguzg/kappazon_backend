4d966f4d51fb2bd0deafa19d6422508d71cc4024
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-07 21:08:33 -0600

    feat(cart): Add Purchase and PurchaseItem models

--- apps/purchases/models.py
+++ apps/purchases/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from ..users.models import User
+from ..products.models import Product
+from .purchaseStates import PURCHASE_STATES
 
-# Create your models here.
+
+class Purchase(models.Model):
+    PURCHASE_CHOICES = [
+        (PURCHASE_STATES['Purchased'], 'Purchased'),
+        (PURCHASE_STATES['Delivered'], 'Delivered'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(
+        max_length=1, choices=PURCHASE_CHOICES, default='P')
+    product_total = models.PositiveIntegerField()
+    shipping_total = models.PositiveIntegerField()
+    products = models.ManyToManyField(Product, through='PurchaseItem')
+
+
+class PurchaseItem(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['purchase', 'product'], name='unique_purchase_item')
+        ]
+
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
