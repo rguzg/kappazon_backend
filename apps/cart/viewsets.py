453b13c9867b5df71ad39329ac3b1b68dc6d291d
Raúl <53493295+rguzg@users.noreply.github.com>
2022-02-07 19:25:25 -0600

    feat(cart): Add CartViewSet route

---
+++ apps/cart/viewsets.py
@@ -0,0 +1,68 @@
+from typing import Any
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from .serializers import CartSerializer
+from .models import CartItem
+from ..products.models import Product
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    """
+    This viewset holds actions related to the Product model
+    """
+
+    serializer_class = CartSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PageNumberPagination
+
+    def destroy(self, request, pk=None):
+        """
+        Overrides the default destroy method. Deletes an item from the current user's cart.
+        """
+        cart = self.get_queryset()
+
+        try:
+            CartItem.objects.get(cart=cart, product=pk).delete()
+            return Response(data=CartSerializer(cart).data, status=200)
+        except:
+            return Response(data="{'error': 'Product doesn't exist in user's cart'}", status=404)
+
+    def create(self, request, pk=None):
+        """
+        Overrides the default create method. Adds/Modifies an item on the current user's cart.
+        """
+        cart = self.get_queryset()
+
+        try:
+            product = Product.objects.get(pk=request.data['product'])
+
+            cart_item, _ = CartItem.objects.get_or_create(
+                cart=cart, product=product, defaults={"cart": cart, "product": product})
+
+            if(request.data['quantity'] == 0):
+                cart_item.delete()
+            else:
+                if(product.is_archived):
+                    return Response(data={"message": "Product is archived"}, status=400)
+
+                if(product.inventory < request.data['quantity']):
+                    return Response(data={"message": "Not enough inventory"}, status=400)
+
+                cart_item.quantity = request.data['quantity']
+                cart_item.save()
+        except:
+            return Response(data="{'error': 'Product doesn't exist'}", status=404)
+
+        cart.total_price += product.price * request.data['quantity']
+        cart.total_items += request.data['quantity']
+
+        return Response(data=CartSerializer(cart).data, status=200)
+
+    def list(self, request):
+        return Response(data=CartSerializer(self.get_queryset()).data, status=200)
+
+    def get_queryset(self):
+        user: Any = self.request.user
+
+        return user.cart
--- kappazon/urls.py
+++ kappazon/urls.py
@@ -21,11 +21,13 @@ from rest_framework import routers
 from apps.users.viewsets import UsersViewSet
 from apps.user.viewsets import UserViewSet
 from apps.products.viewsets import ProductViewSet
+from apps.cart.viewsets import CartViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register('users', UsersViewSet, 'users')
 router.register('user', UserViewSet, 'user')
 router.register('products', ProductViewSet, 'products')
+router.register('cart', CartViewSet, 'cart')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
